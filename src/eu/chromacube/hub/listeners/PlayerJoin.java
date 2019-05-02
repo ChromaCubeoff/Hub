8e2833ca8e7a1ce18dd22faf06c4d777701298a4
MaximeBOHER <47416460+MaximeBOHER@users.noreply.github.com>
2019-05-02 21:49:08 +0200

    UPD: class PlayerJoin
    Update class PlayerJoin

--- src/eu/chromacube/hub/listeners/PlayerJoin.java
+++ src/eu/chromacube/hub/listeners/PlayerJoin.java
@@ -1,6 +1,18 @@
 package eu.chromacube.hub.listeners;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoin implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        player.sendMessage("Ceci est un test !");
+        player.setFoodLevel(20);
+        player.setLevel(0);
+    }
 }
